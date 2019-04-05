1f293b1962f3e2852306dcaf7c065ae664a9834b
shellenorteney <shellen.orteney@estudante.ifms.edu.br>
2019-04-05 13:42:37 -0400

    Atualizando Heroku

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 8080;
-app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.sendfile('./views/index.html');
 });
 
 app.listen(port, function() {
